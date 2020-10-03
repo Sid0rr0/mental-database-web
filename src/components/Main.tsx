44a06178cc48fa2d714f680057c68e45170add24
Sid0rr0 <elhungus@gmail.com>
2020-10-03 21:21:54 +0200

    Fixed padding

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,7 +5,7 @@ const Main = (props: StackProps) => (
 		as="main"
 		width="100%"
 		minH="100vh"
-		py={["10rem", "8em", "10em", "12em"]}
+		py={["10rem", "11rem", "12rem", "13rem"]}
 		{...props}
 		background="repeating-linear-gradient(180deg, #F9F09B, #29ABE2, #F9F09B 1400px )"
 	/>

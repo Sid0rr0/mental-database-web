63cc9619cb21d33e8f033f639085b915b5c98cb2
Sid0rr0 <elhungus@gmail.com>
2020-10-05 10:39:09 +0200

    Better icons

--- src/components/questionaire/SpecialistButton.tsx
+++ src/components/questionaire/SpecialistButton.tsx
@@ -39,7 +39,41 @@ const SpecialistButton: React.FC<SpecialistButtonProps> = ({
 					w="1em"
 					_focus={{ outline: "none" }}
 				>
-					{show ? "^" : "v"}
+					{show ? (
+						<Box w="1.2em">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 84.58 38.02"
+							>
+								<title>arrow up-02</title>
+								<path
+									d="M8.1,69.2,45.7,33.6a5.69,5.69,0,0,1,7.7-.1L92,69.2"
+									transform="translate(-7.76 -31.55)"
+									fill="none"
+									stroke="#000"
+									strokeWidth="4px"
+									strokeMiterlimit="10"
+								/>
+							</svg>
+						</Box>
+					) : (
+						<Box w="1.2em">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 84.58 38.02"
+							>
+								<title>arrow down</title>
+								<path
+									d="M92,31.4,54.4,67a5.69,5.69,0,0,1-7.7.1L8.1,31.4"
+									transform="translate(-7.76 -31.03)"
+									fill="none"
+									stroke="#000"
+									strokeWidth="4px"
+									strokeMiterlimit="10"
+								/>
+							</svg>
+						</Box>
+					)}
 				</Box>
 			</Box>
 			<Collapse isOpen={show}>
